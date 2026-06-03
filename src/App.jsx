ba128a65271eee2a6b83f71f25c9acb7ef01ba1b
Sumitydv45 <sy0050854@gmail.com>
2026-06-03 11:58:40 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RootLayout from "./layout/Rootlayout";
+import Home from "./pages/Home";
+import Shop_ from "./pages/Shop";
+import ProductDetail from "./pages/ProductDetail";
+import Checkout from "./pages/Checkout";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css'
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import RootLayout from './Layout/Rootlayout'
 
 function App() {
- 
-
   return (
-    <>
-    <RootLayout/>
-    {/* <About/> */}
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="Shop" element={<Shop_ />} />
+          <Route path="Product" element={<ProductDetail />} />
+          <Route path="Checkout" element={<Checkout />} />
+
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
